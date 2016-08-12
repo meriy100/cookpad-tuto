92ab8fb37b908884c65cc81b600cb75efb4569ae
kota-kariyado <kota-kariyado@cookpad.com>
2016-08-12 16:08:59 +0900

    recomended spec

--- spec/features/show_item_spec.rb
+++ spec/features/show_item_spec.rb
@@ -8,5 +8,13 @@ RSpec.feature "See shop items", type: :feature do
     expect(page).to have_text('包丁')
     expect(page).to have_text('フライパン')
   end
+
+  scenario 'A user can see recomended items' do
+    create(:item, name: "包丁", recomended: true)
+    create(:item, name: "フライパン", recomended: true)
+    visit recomended_items_path
+    expect(page).to have_text('包丁')
+    expect(page).to have_no_text('フライパン')
+  end
 end
 
