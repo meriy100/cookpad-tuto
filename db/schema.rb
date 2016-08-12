8ae99f1f12c3eaab95a177f57c478f7a52dae40c
kota-kariyado <kota-kariyado@cookpad.com>
2016-08-12 17:12:31 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812073746) do
+ActiveRecord::Schema.define(version: 20160812081216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160812073746) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.boolean  "recommended", default: false, null: false
+    t.integer  "category_id"
   end
 
 end
