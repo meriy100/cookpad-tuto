6e0f7e904e4e92958fa50c37464fe66ba92b8498
kota-kariyado <kota-kariyado@cookpad.com>
2016-08-12 17:09:33 +0900

    add spec and peding

--- spec/features/show_item_spec.rb
+++ spec/features/show_item_spec.rb
@@ -10,15 +10,11 @@ RSpec.feature "See shop items", type: :feature do
   end
 
   scenario 'A user can see recommended items' do
-    create_list(:item, 10, name: "包丁", recommended: true)
+    create(:item, name: "包丁", recommended: true)
     create(:item, name: "フライパン", recommended: false)
     visit recommended_items_path
     expect(page).to have_text('包丁')
     expect(page).not_to have_text('フライパン')
-
-    # expect(page).to have_link '会員登録はこちら'
-    # click_link 'more'
-
   end
 end
 
