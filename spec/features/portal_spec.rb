9bb37cc30a9db97b8ca0753403e1594731158125
kota-kariyado <kota-kariyado@cookpad.com>
2016-08-12 17:39:22 +0900

    uncategorized

--- spec/features/portal_spec.rb
+++ spec/features/portal_spec.rb
@@ -32,6 +32,24 @@ RSpec.feature "See shop categories", type: :feature do
 
     expect(page).not_to have_text('大皿')
   end
+
+  scenario 'A user can see uncategorized items from portal', type: :feature do
+    kitchenware = create(:category, name: '調理器具')
+    kitchenware.items.create!(name: '包丁', recommended: true)
+    kitchenware.items.create!(name: 'フライパン')
+
+    tableware   = create(:category, name: '食器')
+    tableware.items.create!(name: '大皿')
+    Item.create!(name: 'ただのもの')
+
+    visit root_path
+    click_on 'uncategorized'
+    expect(page).not_to have_text('包丁')
+    expect(page).not_to have_text('フライパン')
+
+    expect(page).not_to have_text('大皿')
+    expect(page).to have_text('ただのもの')
+  end
 end
 
 
